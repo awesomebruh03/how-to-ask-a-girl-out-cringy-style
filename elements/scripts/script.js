7c836862ae329b4f8058f3695b52ac6ff393b53e
awesomebruh03 <2001sajidullah@gmail.com>
2024-03-03 12:36:22 +0600

    Initial project setup with all boilerplate files and folders.

---
+++ elements/scripts/script.js
@@ -0,0 +1,19 @@
+const answers_no = {
+    english: [
+        "No",
+        "please forgive me",
+        "i have no idea what ive done",
+        "please",
+        "if you dont forgive me im gonna cry",
+        "what have i done to make you this much mad:(",
+        "Why are you being so cold?",
+        "Maybe we can talk about it?",
+        "I crying :( :(",
+        "you'er hurting my feelings!",
+        ":( :( :( ",
+        "please dont be mad",
+        "Please give me a chance!",
+        "I am begging you to forgive me!",
+        "NO NO NO NO NO NO NO NO NO NO NO NO N ON NO"
+    ],
+};
